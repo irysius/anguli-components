89479ac3dafb8b685f82dc137f9221cd01939a24
irysius <lestersy@hotmail.com>
2018-06-16 15:48:53 -0400

    Prepping 0.2.0

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,12 @@
-// ControllerRouter
-// ErrorHandler
-// helpers
-// Hub
-// HubRouter
+// This is the primary file for usage with nodejs.
+import * as _ControllerRouter from './ControllerRouter';
+import * as _ErrorHandler from './ErrorHandler';
+import * as _helpers from './helpers';
+import * as _Hub from './Hub';
+import * as _HubRouter from './HubRouter';
+
+export let ControllerRouter = _ControllerRouter;
+export let ErrorHandler = _ErrorHandler;
+export let helpers = _helpers;
+export let Hub = _Hub;
+export let HubRouter = _HubRouter;
