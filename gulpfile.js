b4aaf8d32baa5822efe6f3ba3744626f33ce784f
irysius <lestersy@hotmail.com>
2018-06-16 15:39:00 -0400

    Update with fixed @irysius/typings-util

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var exec = require('child_process').exec;
 var clean = require('gulp-clean');
 var sequence = require('run-sequence');
+var generateDeclaration = require('@irysius/typings-util');
 
 // compile - compiles typescript into javascript
 gulp.task('compile', (done) => {
@@ -26,9 +27,43 @@ gulp.task('clean', () => {
     ]).pipe(clean());
 });
 
+// declaration-compile - generates .d.ts from the TypeScript files
+gulp.task('declaration-compile', (done) => {
+    exec('node-tsc -d', (err, stdout, stderr) => {
+        done(); // continue even if there's errors.
+    });
+});
+
+// declaration-copy - copies .d.ts to the types folder
+gulp.task('declaration-copy', () => {
+    return gulp.src([
+        'src/**/*.d.ts'
+    ]).pipe(gulp.dest('types'));
+});
+
+// declaration-clean - removes unwanted .d.ts, and removes .d.ts from the src folder
+gulp.task('declaration-clean', () => {
+    return gulp.src([
+        'main.d.ts',
+        'check.d.ts',
+        'src/**/*.d.ts',
+        'tests/**/*.d.ts',
+        'types/**/*.d.ts'
+    ]).pipe(clean());
+});
+
+// declaration-generate - generates a unified declaration
+gulp.task('declaration-generate', () => {
+    return generateDeclaration('./types', '@irysius/anguli-components', './index.d.ts');
+});
+
 gulp.task('build', (done) => {
     sequence('compile', 'copy', 'clean', done);
 });
 
-gulp.task('default', ['build']);
+gulp.task('build-declaration', (done) => {
+    sequence('declaration-compile', 'declaration-copy', 'declaration-generate', 'declaration-clean', done);
+});
+
+gulp.task('default', ['build', 'build-declaration']);
 gulp.task('prepare', ['build']);
